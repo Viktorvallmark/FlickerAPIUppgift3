cdfc657e203986cc375a22e715fe39ecb1aaecd7
Viktorvallmark <91593947+Viktorvallmark@users.noreply.github.com>
2022-02-08 11:06:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,40 +2,36 @@ const btn = document.querySelector('button');
 const KEY = '80f504ef0b8c81509970c71f21e8813c';
 console.log(btn);
 
+const size = document.getElementById('size');
+const checkSize = size.options[size.selectedIndex].value;
+console.log(checkSize);
 
-
-
-btn.addEventListener('click', function (event) {
+btn.addEventListener('click', function(event) {
+    event.preventDefault();
     const searchField = document.querySelector('input');
     console.log(searchField.value);
-    const size = document.getElementById('size');
-    const checkSize = size.options[size.selectedIndex].value;
-    console.log(checkSize);
-    
+   
     removeImg();
 
-    const url = `https://www.flickr.com/services/rest/
-    ?method=flickr.photos.search&api_key=${KEY}&text=${searchField.value}&format=json&nojsoncallback=1
-    &per_page=1&page=1`;
+    const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchField.value}&format=json&nojsoncallback=1&per_page=1&page=1`;
     
-    fetch(url).then(function (response) {
-        console.log(response);
-        if (response.status >= 200 && response.status < 300) {
-            return response.json();
-        } else {
-
-            throw 'HTTPS status Error!'
-        }
-        }).then(data => {console.log(data); imageURL(data.photos.photo[0])}).catch( handleError );
+    let k = fetch(url).then(handleResponse).then(buildURL).then(displayImage).catch(handleError);
+    console.log(k);
     
 });
 
-function imageURL(photoObject){
-    let sizeValue = checkSize;
-    let photo = photoObject;
-    
-    let imgURL = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${sizeValue}.jpg`;
-    displayImage(imgURL);
+function handleResponse(response){
+    if (response.status >= 200 && response.status < 300) {
+        return response.json();
+    } else {
+        throw 'Error!'
+    }
+}
+
+function buildURL(data){
+    let size = checkSize;
+    let imgURL = `https://live.staticflickr.com/${data.photos.photo[0].server}/${data.photos.photo[0].id}_${data.photos.photo[0].secret}_${size}.jpg`;
+    return imgURL;
 }
 
 function displayImage(url){
@@ -46,16 +42,11 @@ function displayImage(url){
 }
 
 function handleError(error){
-    if(error === 'HTTPS status Error!'){
-        const p = document.createElement('p');
-        p.innerText = `${response.status} a HTTPS status Error occured!`;
-        document.body.append(p);
-    }else{
-        const p1 = document.createElement('p');
-        p1.innerText = `'You didn't choose a size on your picture!`;
-        document.body.append(p1);
+    if(error){
+        const h2 = document.createElement('h2');
+        h2.innerText = `Something went wrong, try a different search query!`;
+        document.body.append(h2);
     }
-   
 }
 
 function removeImg() {
